f197e1e21533ca589975307b6a7141adeb9b7841
Stone <stone@Stones-MacBook-Pro.local>
2017-07-26 20:16:01 +0700

    change brain CHANNEL to develop branch

--- configs.js
+++ configs.js
@@ -1,5 +1,5 @@
 const SUB_TIME_OUT = 10000
-const SENSORS_PUSH_PULL = "ipc:///tmp/brain_zmq_sensor_push_pull"
+const SENSORS_PUSH_PULL = "ipc:///tmp/brain_zmq_sensor"
 const PUB_SENSOR_PUB_SUB = "ipc:///tmp/brain_zmq_sensor_pub_sub"
 
 module.exports = {
